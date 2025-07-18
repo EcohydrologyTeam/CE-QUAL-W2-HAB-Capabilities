717026f087f71d5a763a55173b7e4cce4fcafecc
Todd Steissberg <45308822+TSteissberg@users.noreply.github.com>
2025-07-18 12:32:59 -0700

    Deleted old model; added new June 26, 2025 version

--- W2_v45_ERDC25coding/W2 model/x64/Release/gregorian_date__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Feb  3 17:00:21 2025
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE GREGORIAN_DATE__genmod
-          INTERFACE 
-            RECURSIVE SUBROUTINE GREGORIAN_DATE
-            END SUBROUTINE GREGORIAN_DATE
-          END INTERFACE 
-        END MODULE GREGORIAN_DATE__genmod
--- W2_v45_ERDC25coding/W2 model/x64/Release/wqconstituents__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Feb  3 17:00:19 2025
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE WQCONSTITUENTS__genmod
-          INTERFACE 
-            RECURSIVE SUBROUTINE WQCONSTITUENTS
-            END SUBROUTINE WQCONSTITUENTS
-          END INTERFACE 
-        END MODULE WQCONSTITUENTS__genmod
